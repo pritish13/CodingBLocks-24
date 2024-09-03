507cb365ec49b4330f1512bf0d32e651be071578
Pritish <pritishgupta1310@gmail.com>
2024-09-03 13:10:34 +0530

    Pattern

--- Lecture0/helloworld.java
+++ Lecture0/helloworld.java
@@ -1,3 +1,4 @@
+
 public class helloworld{
     public static void main(String[] args){
         System.out.print("hello world");
---
+++ lecture1/pattern1.java
@@ -0,0 +1,14 @@
+package lecture1;
+
+public class pattern1 {
+    public static void main(String[] args){
+        //pseudo code - code i.e. logically correct but not acc. to syntax
+         
+     int n=3;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" "); // will give a sq. 
+        }
+    }
